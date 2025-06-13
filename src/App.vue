bf8d0b446c45461dd502c32ddb88cefe2873f30c
Josh <joshuaepenner@gmail.com>
2025-06-13 10:45:35 -0500

    Remove more boilerplate and add header sizing

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-    <header>
-    </header>
-    <RouterView />
+    <div class="h-screen flex flex-col">
+        <header class="w-full h-20">
+        </header>
+        <main class="w-full h-full">
+            <div class="w-full h-full">
+                <RouterView />
+            </div>
+        </main>
+    </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,5 @@
 </script>
 
 <template>
-    <div>
-        <h1>This is a home page</h1>
-    </div>
+    <h1>This is a home page</h1>
 </template>
